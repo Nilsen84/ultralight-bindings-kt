512f2a6c21a1fc0d4eeb1579480caa6586e4b16e
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2026-02-14 17:07:26 +0100

    Update GitHub Packages URL in ultralight-publishing script

--- buildSrc/src/main/kotlin/ultralight-publishing.gradle.kts
+++ buildSrc/src/main/kotlin/ultralight-publishing.gradle.kts
@@ -6,7 +6,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/nilsen84/ultralight-bindings")
+            url = uri("https://maven.pkg.github.com/nilsen84/ultralight-bindings-kt")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
